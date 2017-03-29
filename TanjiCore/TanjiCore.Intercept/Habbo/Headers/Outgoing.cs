ef72760e3fd53e4b75522565bd22fa9e035f2584
Scott Stamp <scottstamp851@gmail.com>
2017-03-29 19:15:07 -0230

    Tidied up usings and some code in TanjiCore.Intercept

--- TanjiCore/TanjiCore.Intercept/Habbo/Headers/Outgoing.cs
+++ TanjiCore/TanjiCore.Intercept/Habbo/Headers/Outgoing.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Runtime.Serialization.Json;
-using System.Text;
 
 namespace TanjiCore.Intercept.Habbo.Headers
 {
